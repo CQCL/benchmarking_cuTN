e119b06457194fcb40fcf1381ac0d229f759f092
Pablo Andres-Martinez <pablo.andresmartinez@quantinuum.com>
2023-12-12 08:51:08 -0800

    Updated code to avoid recomputing data points obtained in previous run

--- itensors_mps.py
+++ itensors_mps.py
@@ -22,14 +22,25 @@ rank = comm.Get_rank()
 n_procs = comm.Get_size()
 
 directory = os.fsencode("Circuits/")
+data_name = f"Results/tmp/itensors_chi_{chi}.dat"
 
-with open(f"Results/tmp/itensors_chi_{chi}.dat_{rank}", "a") as data:
+# Figure out which circuits have already been simulated
+already_simulated = []
+for r in range(n_procs):
+    with open(f"{data_name}_{r}", "r") as f:
+        lines = f.readlines()
+        already_simulated += [l.split()[0] for l in lines]
+
+# Simulate the rest
+with open(f"{data_name}_{rank}", "a") as data:
   for k, file in enumerate(os.listdir(directory)):
     if k % n_procs != rank: continue  # Skip if this process is not responsible of simulating this circuit
 
     filename = os.fsdecode(file)
     if not filename.endswith(".json"):
         raise RuntimeError(f"File {filename} in the Circuits/ directory is not a json file.")
+
+    if filename in already_simulated: continue  # Skip if circuit has already been simulated
 
     print(f"Simulating {filename}")
     sys.stdout.flush()
