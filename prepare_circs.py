11444a040b0ea13e7eac5070b20cb83d1745a280
Pablo Andres-Martinez <pablo.andresmartinez@quantinuum.com>
2024-04-23 03:23:05 -0700

    Changed prepare_circs.py so that it adds dummy ZZPhase gates at the beginning of the circuit to circumvent a bug in ITensorGPU.

--- prepare_circs.py
+++ prepare_circs.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 
 import cirq
-from pytket.circuit import OpType
+from pytket.circuit import OpType, Circuit, Qubit
 from pytket.extensions.cirq import cirq_to_tk
 from pytket.extensions.cutensornet.structured_state import prepare_circuit_mps
 
@@ -15,8 +15,15 @@ for i, file_path in enumerate(directory_path.iterdir()):
 
     circ = cirq_to_tk(cirq.read_json("Circuits/"+filename))
     circ, _ = prepare_circuit_mps(circ)
+    circ.rename_units({Qubit("node", i): Qubit(i) for i in range(circ.n_qubits)})
+
+    # Add ZZPhase gates with angle 0 to avoid ITensorsGPU bug
+    pre_circ = Circuit(circ.n_qubits)
+    for q0, q1 in zip(circ.qubits[:-1], circ.qubits[1:]):
+        pre_circ.ZZPhase(0.0, q0, q1)
+    pre_circ.add_circuit(circ, circ.qubits)
 
     with open("tket_circuits/"+filename, "w") as f:
-        json.dump(circ.to_dict(), f, indent=2)
+        json.dump(pre_circ.to_dict(), f, indent=2)
 
     print(f"{i+1}/480")
