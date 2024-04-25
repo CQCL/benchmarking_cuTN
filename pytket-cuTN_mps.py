afb365c4878037e8826e82b812acf2d37316aa65
Pablo Andres-Martinez <pablo.andresmartinez@quantinuum.com>
2024-04-25 03:41:53 -0700

    Updated our approach to use the same selected circuits

--- pytket-cuTN_mps.py
+++ pytket-cuTN_mps.py
@@ -1,63 +1,64 @@
-import os
 import sys
+from pathlib import Path
 import json
 import time as t
 
 from mpi4py import MPI
 
-import cirq
-from pytket.extensions.cirq import cirq_to_tk
-from pytket.extensions.cutensornet.mps import ContractionAlg, simulate, prepare_circuit, ConfigMPS, CuTensorNetHandle
+from pytket import Circuit
+from pytket.extensions.cutensornet.structured_state import SimulationAlgorithm, simulate, Config, CuTensorNetHandle
 
 comm = MPI.COMM_WORLD
 
-chi = int(sys.argv[1])
+if sys.argv[1] != "OURS":
+    raise ValueError("If calling pytket-cutensornet method use OURS as the first argument.")
+
+trunc_mode = sys.argv[2]
+param = sys.argv[3]
+
+if trunc_mode == "chi":
+    chi = int(param)
+    truncation_fidelity = None
+elif trunc_mode == "trunc_error":
+    trunc_error = float(param)
+    assert trunc_error >= 0 and trunc_error <= 1
+    truncation_fidelity = 1 - trunc_error
+    chi = None
+else:
+    raise ValueError("Use either `chi` or `trunc_error`.")
+
 rank = comm.Get_rank()
 n_procs = comm.Get_size()
 
-directory = os.fsencode("Circuits/")
-data_name = f"Results/tmp/pytket-cutn_chi_{chi}.dat"
+directory = Path("selected_circs/")
 
-# Figure out which circuits have already been simulated
-already_simulated = []
-for r in range(n_procs):
-    try:
-        with open(f"{data_name}_{r}", "r") as f:
-            lines = f.readlines()
-            already_simulated += [l.split()[0] for l in lines]
-    except FileNotFoundError:
-        continue
-
-# Simulate the rest
-with open(f"{data_name}_{rank}", "a") as data:
-  for k, file in enumerate(os.listdir(directory)):
+with open("our_results.csv", "a") as data:
+  for k, file_path in enumerate(directory.iterdir()):
     if k % n_procs != rank: continue  # Skip if this process is not responsible of simulating this circuit
 
-    filename = os.fsdecode(file)
-    if not filename.endswith(".json"):
-        raise RuntimeError(f"File {filename} in the Circuits/ directory is not a json file.")
+    with open(file_path, "r") as f:
+        circ = Circuit.from_dict(json.load(f))
 
-    if filename in already_simulated: continue  # Skip if circuit has already been simulated
+    with CuTensorNetHandle(rank) as libhandle:
+        cfg = Config(
+            chi=chi,
+            truncation_fidelity=truncation_fidelity,
+            value_of_zero=0,
+        )
+        try:
+            start_time = t.time()
+            mps = simulate(libhandle, circ, SimulationAlgorithm.MPSxGate, cfg)
+            duration = t.time() - start_time
+            fidelity = mps.fidelity
 
-    print(f"Simulating {filename}")
-    sys.stdout.flush()
+            filename = str(file_path).split("/")[-1]
+            entry = f"{filename},OURS,{trunc_mode},{param},{duration},{fidelity}\n"
+            data.write(entry)
+            data.flush()
+            print(entry)
+            sys.stdout.flush()
 
-    circ = cirq_to_tk(cirq.read_json("Circuits/"+filename))
-    circ, _ = prepare_circuit(circ)
-
-    try:
-      start_time = t.time()
-      with CuTensorNetHandle(rank) as libhandle:
-          cfg = ConfigMPS(chi=chi)
-          mps = simulate(libhandle, circ, ContractionAlg.MPSxGate, cfg)
-      duration = t.time() - start_time
-      fidelity = mps.fidelity
-
-      entry = f"{filename} {duration} {fidelity}\n"
-      data.write(entry)
-      data.flush()
-
-    except Exception as e:
-      print(f"Failed! {filename}, {e}")
-      data.write(f"{filename} nan nan\n")
-      data.flush()
+        except Exception as e:
+            print(f"Failed! {filename}, {e}")
+            data.write(f"{filename},OURS,{trunc_mode},{param},nan,nan\n")
+            data.flush()
